c1ed0f634867bcb610c4edfa682e6cb75bae6cd0
jun rogan <junrogan@jundeMacBook-Pro.local>
2022-11-22 00:19:41 +0800

    init commit

---
+++ core.go
@@ -0,0 +1,184 @@
+package merkle_tree
+
+import (
+	"bytes"
+	"errors"
+	"hash"
+	"math"
+)
+
+const DigestLength = 32
+
+type Node []byte
+
+type Tree struct {
+	Hasher hash.Hash
+	Nodes  []Node
+}
+
+func (tree *Tree) hashPair(a Node, b Node) []byte {
+	tree.Hasher.Reset()
+	buff := bytes.Buffer{}
+	if bytes.Compare(a[:], b[:]) != 1 {
+		buff.Write(a[:])
+		buff.Write(b[:])
+	} else {
+		buff.Write(b[:])
+		buff.Write(a[:])
+	}
+	tree.Hasher.Write(buff.Bytes())
+	return tree.Hasher.Sum(nil)
+}
+
+func leftChildIndex(i int) int {
+	return 2*i + 1
+}
+
+func rightChildIndex(i int) int {
+	return 2*i + 2
+}
+
+func parentIndex(i int) (int, error) {
+	if i == 0 {
+		return 0, errors.New("root has no parent")
+	}
+
+	return int(math.Floor((float64(i) - 1) / 2)), nil
+}
+
+func siblingIndex(i int) (int, error) {
+	if i == 0 {
+		return 0, errors.New("root has no siblings")
+	}
+
+	if i%2 == 0 {
+		return i - 1, nil
+	} else {
+		return i + 1, nil
+	}
+}
+
+func isTreeNode(tree *Tree, i int) bool {
+	return i >= 0 && i < len(tree.Nodes)
+}
+
+func isInternalNode(tree *Tree, i int) bool {
+	return isTreeNode(tree, leftChildIndex(i))
+}
+
+func isLeafNode(tree *Tree, i int) bool {
+	return isTreeNode(tree, i) && !isInternalNode(tree, i)
+}
+
+func isValidMerkleNode(node Node) bool {
+	return len([]byte(node)) == DigestLength
+}
+
+func isPowerOf2(num int) bool {
+	return num&(num-1) == 0
+}
+
+func NewMerkeTree(hasher hash.Hash, leaves []Node) (*Tree, error) {
+	leavesLength := len(leaves)
+	if leavesLength == 0 {
+		return nil, errors.New("empty leaves")
+	}
+
+	if !isPowerOf2(len(leaves)) {
+		return nil, errors.New("only support complete binary tree")
+	}
+
+	for i := 0; i < leavesLength; i++ {
+		if !isValidMerkleNode(leaves[i]) {
+			return nil, errors.New("node's byte length is not 32")
+		}
+	}
+
+	tree := &Tree{
+		Hasher: hasher,
+		Nodes:  make([]Node, 2*leavesLength-1),
+	}
+
+	for i, leaf := range leaves {
+		tree.Nodes[leavesLength-1+i] = leaf
+	}
+
+	for i := leavesLength - 2; i >= 0; i-- {
+		tree.Nodes[i] = tree.hashPair(tree.Nodes[leftChildIndex(i)], tree.Nodes[rightChildIndex(i)])
+	}
+
+	return tree, nil
+}
+
+func getProofByIndex(tree *Tree, i int) ([]Node, error) {
+	if !isLeafNode(tree, i) {
+		return nil, errors.New("not a leaf node")
+	}
+
+	proof := make([]Node, 0)
+	for i > 0 {
+		sibIndex, _ := siblingIndex(i)
+		proof = append(proof, tree.Nodes[sibIndex])
+		i, _ = parentIndex(i)
+	}
+	return proof, nil
+}
+
+func getProof(tree *Tree, leaf Node) ([]Node, error) {
+	leafBeginIndex := len(tree.Nodes) / 2
+
+	found := false
+	i := leafBeginIndex
+	for ; i < len(tree.Nodes); i++ {
+		if bytes.Compare(leaf, tree.Nodes[i]) == 0 {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return nil, errors.New("not a leaf node")
+	}
+
+	return getProofByIndex(tree, i)
+}
+
+func processProof(tree *Tree, leaf Node, proof []Node) (Node, error) {
+	if !isValidMerkleNode(leaf) {
+		return nil, errors.New("not a merkle node ")
+	}
+
+	for i := 0; i < len(proof); i++ {
+		if !isValidMerkleNode(proof[i]) {
+			return nil, errors.New("not a merkle node ")
+		}
+	}
+
+	node := leaf
+	for i := 0; i < len(proof); i++ {
+		node = tree.hashPair(node, proof[i])
+	}
+
+	return node, nil
+}
+
+func isValidMerkeTree(tree *Tree) bool {
+	treeLength := len(tree.Nodes)
+	for i, node := range tree.Nodes {
+		if !isValidMerkleNode(node) {
+			return false
+		}
+
+		l := leftChildIndex(i)
+		r := rightChildIndex(i)
+
+		if r >= treeLength {
+			if l < treeLength {
+				return false
+			}
+		} else if bytes.Compare(node, tree.hashPair(tree.Nodes[l], tree.Nodes[r])) != 0 {
+			return false
+		}
+	}
+	return treeLength > 0
+}
---
+++ core_test.go
@@ -0,0 +1,192 @@
+package merkle_tree
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/sha3"
+	"testing"
+)
+
+func generateLeaves(num int) []Node {
+	leaves := make([]Node, num)
+
+	for i := 0; i < num; i++ {
+		hasher := sha3.NewLegacyKeccak256()
+		hasher.Write([]byte(fmt.Sprint(i)))
+		leaves[i] = hasher.Sum(nil)
+	}
+	return leaves
+}
+
+func TestSiblingIndex(t *testing.T) {
+	sib, err := siblingIndex(0)
+	require.Error(t, err)
+
+	sib, err = siblingIndex(1)
+	require.NoError(t, err)
+	require.Equal(t, 2, sib)
+
+	sib, err = siblingIndex(2)
+	require.NoError(t, err)
+	require.Equal(t, 1, sib)
+
+	sib, err = siblingIndex(3)
+	require.NoError(t, err)
+	require.Equal(t, 4, sib)
+
+	sib, err = siblingIndex(4)
+	require.NoError(t, err)
+	require.Equal(t, 3, sib)
+
+	sib, err = siblingIndex(5)
+	require.NoError(t, err)
+	require.Equal(t, 6, sib)
+
+	sib, err = siblingIndex(6)
+	require.NoError(t, err)
+	require.Equal(t, 5, sib)
+
+	sib, err = siblingIndex(7)
+	require.NoError(t, err)
+	require.Equal(t, 8, sib)
+
+	sib, err = siblingIndex(13)
+	require.NoError(t, err)
+	require.Equal(t, 14, sib)
+}
+
+func TestParentIndex(t *testing.T) {
+	parent, err := parentIndex(0)
+	require.Error(t, err)
+
+	parent, err = parentIndex(1)
+	require.NoError(t, err)
+	require.Equal(t, 0, parent)
+
+	parent, err = parentIndex(2)
+	require.NoError(t, err)
+	require.Equal(t, 0, parent)
+
+	parent, err = parentIndex(3)
+	require.NoError(t, err)
+	require.Equal(t, 1, parent)
+
+	parent, err = parentIndex(4)
+	require.NoError(t, err)
+	require.Equal(t, 1, parent)
+
+	parent, err = parentIndex(5)
+	require.NoError(t, err)
+	require.Equal(t, 2, parent)
+
+	parent, err = parentIndex(6)
+	require.NoError(t, err)
+	require.Equal(t, 2, parent)
+
+	parent, err = parentIndex(7)
+	require.NoError(t, err)
+	require.Equal(t, 3, parent)
+
+	parent, err = parentIndex(13)
+	require.NoError(t, err)
+	require.Equal(t, 6, parent)
+}
+
+//0:044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d
+//1:c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6
+//2:ad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5
+//3:2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de
+func TestHasher(t *testing.T) {
+	for i := 0; i < 4; i++ {
+		msg := []byte(fmt.Sprint(i))
+
+		//eth keccak256
+		h1 := crypto.Keccak256(msg)
+
+		//sha3.keccak256
+		hasher := sha3.NewLegacyKeccak256()
+		hasher.Write(msg)
+		h2 := hasher.Sum(nil)
+
+		require.Equal(t, h1, h2)
+	}
+}
+
+func TestNewMerkleTree(t *testing.T) {
+	leaves := generateLeaves(4)
+	tree, err := NewMerkeTree(sha3.NewLegacyKeccak256(), leaves)
+	require.NoError(t, err)
+
+	proof, err := getProofByIndex(tree, 3)
+	require.NoError(t, err)
+
+	root, err := processProof(tree, tree.Nodes[3], proof)
+	require.NoError(t, err)
+	require.Equal(t, root, tree.Nodes[0])
+
+	root, err = processProof(tree, leaves[0], proof)
+	require.NoError(t, err)
+	require.Equal(t, root, tree.Nodes[0])
+}
+
+func TestNewMerkleTree_EmptyLeaves(t *testing.T) {
+	_, err := NewMerkeTree(sha3.NewLegacyKeccak256(), nil)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "mpty leaves")
+}
+
+func TestGetProof(t *testing.T) {
+	leaves := generateLeaves(4)
+	tree, err := NewMerkeTree(sha3.NewLegacyKeccak256(), leaves)
+	require.NoError(t, err)
+
+	proof, err := getProof(tree, leaves[0])
+	require.NoError(t, err)
+	fmt.Printf("leaf:%x,\nproof:%x,\nroot:%x\n", leaves[0], proof, tree.Nodes[0])
+
+	root, err := processProof(tree, tree.Nodes[3], proof)
+	require.NoError(t, err)
+	require.Equal(t, root, tree.Nodes[0])
+
+	root, err = processProof(tree, leaves[0], proof)
+	require.NoError(t, err)
+	require.Equal(t, root, tree.Nodes[0])
+}
+
+func TestHashPair(t *testing.T) {
+	a, err := hex.DecodeString("044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d")
+	require.NoError(t, err)
+	b, err := hex.DecodeString("c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6")
+	require.NoError(t, err)
+
+	tree := Tree{
+		Hasher: sha3.NewLegacyKeccak256(),
+	}
+	h1 := tree.hashPair(a, b)
+	h2 := tree.hashPair(b, a)
+	require.Equal(t, h1, h2)
+}
+
+func TestIsValidMerkleTree(t *testing.T) {
+	leaves := generateLeaves(4)
+	tree, err := NewMerkeTree(sha3.NewLegacyKeccak256(), leaves)
+	require.NoError(t, err)
+
+	require.True(t, isValidMerkeTree(tree))
+
+	tree = &Tree{}
+	require.False(t, isValidMerkeTree(tree))
+
+	tree = &Tree{
+		Nodes: []Node{leaves[0], leaves[1]},
+	}
+	require.False(t, isValidMerkeTree(tree))
+
+	tree = &Tree{
+		Hasher: sha3.NewLegacyKeccak256(),
+		Nodes:  []Node{leaves[0], leaves[1], leaves[2]},
+	}
+	require.False(t, isValidMerkeTree(tree))
+}
